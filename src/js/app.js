5b9c240bc75338933463a318cc5182b73c03e2c1
pblfer <pblfertec@gmail.com>
2017-05-10 21:03:45 -0600

    React DevServer

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,8 @@
   const css = require('../css/main.scss');
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+
+  ReactDOM.render(
+    <h1>Hello, world!</h1>,
+    document.getElementById('root')
+  );
--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,17 @@
         path: path.resolve(__dirname, 'dist'),
         filename: 'pack.bundle.js'
     },
+        devServer: {
+          contentBase: path.join(__dirname, "dist"),
+          compress: true,
+          port: 9000,
+          stats: "errors-only",
+          open: true
+        },
       module: {
           rules: [
-            {test: /\.scss$/, use: ['style-loader','css-loader', 'sass-loader']}
+            {test: /\.scss$/, use: ['style-loader','css-loader', 'sass-loader']},
+            {test: /\.js$/, exclude:/node_modules/, use: 'babel-loader'}
           ]
       },
     plugins: [
